04dc7a04c56698ca3929ab6302b4a6152490bdf5
syun <konnnichiha0501@gmail.com>
2020-02-05 17:56:42 +0900

    jjjjjj

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,43 @@
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
+
+
+function OpenMOT()
+	{document.getElementById("MOT").classList.add("rotate-box")}
+
+function CloseMOT()
+	{document.getElementById("MOT").classList.remove("rotate-box")}
+
+var slideIndex = 1;
+    showSlides(slideIndex);
+function showSlides(n){
+   var i;
+   var slides = document.getElementsByClassName("data-for-thumbnail");
+   if(n > slides.length){slideIndex = 1}
+   if(n < 1){slideIndex = slides.length}
+   for(i = 0; i < slides.length; i++){
+       slides[i].style.display = "none"
+   }
+   slides[slideIndex-1].style.display = "block"
+}
+
+var carousel = document.querySelector(".people");
+var cellCount = 8;
+var selectedIndex = 0;
+function getCarousel(){
+   var angle = selectedIndex / cellCount * -360;
+   carousel.style.transform = "rotateY(" + angle + "deg)"
+}
+
+function prev(n){
+   showSlides(slideIndex += n);
+   selectedIndex--;
+   getCarousel()
+}
+
+function next(n){
+   showSlides(slideIndex += n);
+   selectedIndex++;
+   getCarousel()
+}
